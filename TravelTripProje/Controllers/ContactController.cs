d858124c250e059679e794f65607b04a01ef6763
Gizem Yıldız <gizeemyildiz1@gmail.com>
2022-09-04 22:34:07 +0300

    code edit

---
+++ TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+
+        DbCtx c = new DbCtx();
+        public ActionResult Index()
+        {
+            var degerler = c.Contacts.ToList();
+            return View(degerler);
+        }
+    }
+}
